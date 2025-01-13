7f8bc96ce72d39d6a17f5448fde9430b347bbd23
Ayusharma95 <ayushtejpalsharma@gmail.com>
2025-01-13 21:10:37 +0530

    icons not added

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,14 +16,6 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <head>
-        {/* Favicon and icons from "favicon_io" */}
-        <link rel="icon" href="/favicon_io/favicon.ico" />
-        <link rel="apple-touch-icon" href="/favicon_io/apple-touch-icon.png" />
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon_io/favicon-32x32.png" />
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicon_io/favicon-16x16.png" />
-        <link rel="manifest" href="/favicon_io/site.webmanifest" />
-      </head>
       <body className={inter.className}>{children}</body>
     </html>
   );
